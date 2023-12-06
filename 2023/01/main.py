8d6ff6d3ac07533eb16a370b65ae294405ea5e00
Ewig Geek <ewig.geek@gmail.com>
2023-12-06 21:25:40 +0000

    feat: trebuchet?! part two

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -1,10 +1,16 @@
 import sys
 import re
 
+numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+regexForNumbers = '(?=((?:f(?:ive|our)|s(?:even|ix)|t(?:hree|wo)|(?:ni|o)ne|eight|[0-9])))'
+
 resultingNumber = 0
 for line in sys.stdin:
-        firstDigit = re.search(r'[0-9]', line).group()
-        secondDigit = re.search(r'[0-9]', line[::-1]).group()
-        resultingNumber += int(firstDigit + secondDigit)
+        digits = re.findall(regexForNumbers, line)
+
+        firstDigit = digits[0] if len(digits[0]) == 1 else str(numbers.index(digits[0]))
+        lastDigit = digits[-1] if len(digits[-1]) == 1 else str(numbers.index(digits[-1]))
+
+        resultingNumber += int(firstDigit + lastDigit)
 
 print(resultingNumber)
